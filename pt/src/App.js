1b3aac9a975542f992f5b012f49255c1e0f24eb6
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-16 21:56:07 +0900

    날씨 api연결

--- pt/src/App.js
+++ pt/src/App.js
@@ -1,47 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  let posts = '강남 고기 맛집';
-  let [title,titleSet] = useState(['남자 코트 추천', '강남 우동 맛집','당산역카페']);
-  let [heart,heartSet] = useState(0);
-
-  function titleChange(){
-    let newtitle = [...title]
-    newtitle[0] = '여자 코트 추천';
-    titleSet(newtitle);
-  }
-  function arrayChange(){
-    let arrayChange = [...title];
-    arrayChange.sort();
-    titleSet(arrayChange);
-  }
+  let [city,citySet] = useState('서울');
+  let [Temperature,TemperatureSet] = useState('3');
+  axios.get('http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=1cb3f6bc10c3213a2c7e8251f0799a12')
+  .then((result)=>{console.log(result.data)});
+  
   return (
     <div className="App">
-        <div className="black-nav"> 
-        <div>개발 blog</div>
-      </div>
-      <div className="list">
-        <h3>{ title[0] }<span onClick={()=>{heartSet(heart+1)}}>❤️</span>{heart}</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{ title[1] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{ title[2] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <button onClick={()=>{titleChange()}}>제목변경</button>
-      <button onClick={()=>{arrayChange()}}>가나다 정렬</button>
+    <div className='background'>
+      <h1 className='title'>오늘{city}의날씨는</h1>
+      <ul>
+        <li><i class="far fa-snowflake"></i></li>
+        <li>눈이내려요</li>
+        <li><span className='Temperature'>{Temperature}</span>℃입니다</li>
+      </ul>
+      <h3>🧣두꺼운 옷을 입으세요🧤<br/>
+      패딩,두꺼운코트,목도리 등
+      </h3>
+      <form>
+        <input type="text" className='where' placeholder="도시 입력" onChange={(e)=>{}}></input>
+        <button onClick={()=>{
+          
+        }}>검색</button>
+      </form>
+    </div>
     </div>
   );
 }
-   /*리액트에서 class=""를 넣고 싶으면 className="" 이라고 사용*/
+/*리액트에서 class=""를 넣고 싶으면 className="" 이라고 사용*/
    //"변수가 변경될 때 자동으로 관련된 HTML을 재렌더링되게 만들고 싶으면"
 export default App;
